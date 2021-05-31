45d0ef1fa6cd4554932d2f4cb8ccf15e412519b0
sunghyuki <sunghyuk1609@gmail.com>
2021-05-31 16:01:38 +0900

    feat: 네이버 클라우드 플랫폼 서버 동작 확인용 커밋

--- server/routes/trailer.js
+++ server/routes/trailer.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Trailer = require('../models/Trailer');
-const bcryptjs = require('bcryptjs');
-const user_jwt = require('../middleware/user_jwt');
+const Trailer = require("../models/Trailer");
+const bcryptjs = require("bcryptjs");
+const user_jwt = require("../middleware/user_jwt");
 
 //트레일러 정보 등록 API
-router.post('/trailers', user_jwt, async(req, res, next) => {
+router.post("/trailers", user_jwt, async (req, res, next) => {
   try {
     const trailer = await Trailer.create({
       userId: req.user.id,
@@ -16,39 +16,38 @@ router.post('/trailers', user_jwt, async(req, res, next) => {
       cost: req.body.cost,
       capacity: req.body.capacity,
       facilities: req.body.facilities,
-      description: req.body.description
+      description: req.body.description,
     });
 
-    if(!trailer) {
+    if (!trailer) {
       res.status(400).json({
         success: false,
-        msg: 'Something went wrong'
+        msg: "Something went wrong",
       });
     }
 
     res.status(200).json({
       success: true,
       trailer: trailer,
-      msg: 'Successfully created'
+      msg: "Successfully created",
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-
 });
 
 //메인화면 모든 트레일러 정보 호출 API
-router.get('/trailers', user_jwt, async(req,res,next) => {
+router.get("/trailers", user_jwt, async (req, res, next) => {
   try {
     const trailers = await Trailer.find();
 
     const user = req.user.id;
     console.log(user);
 
-    if(trailers.length == 0) {
+    if (trailers.length == 0) {
       res.status(400).json({
         success: false,
-        msg: 'Trailer not exists'
+        msg: "Trailer not exists",
       });
     }
 
@@ -57,23 +56,23 @@ router.get('/trailers', user_jwt, async(req,res,next) => {
       totalNumberOfTrailers: trailers.length,
       user: user,
       trailer: trailers,
-      msg: 'Successfully fetched'
+      msg: "Successfully fetched",
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //트레일러 선택시 트레일러에 대한 상세 정보 확인 API
-router.get('/trailers/:id', user_jwt, async(req, res, next) => {
+router.get("/trailers/:id", user_jwt, async (req, res, next) => {
   try {
     let trailer = await Trailer.findById(req.params.id);
     const user = req.user.id;
 
-    if(!trailer) {
+    if (!trailer) {
       res.status(400).json({
         success: false,
-        msg: 'trailerInfo not exists'
+        msg: "trailerInfo not exists",
       });
     }
 
@@ -81,22 +80,22 @@ router.get('/trailers/:id', user_jwt, async(req, res, next) => {
       success: true,
       user: user,
       trailer: trailer,
-      msg: 'Successfully fetched'
+      msg: "Successfully fetched",
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-})
+});
 
 //등록한 트레일러 삭제 API - 문제 발생
-router.delete('/trailers/:id', user_jwt, async(req, res, next) => {
+router.delete("/trailers/:id", user_jwt, async (req, res, next) => {
   try {
     let registeredTrailer = await Trailer.findById(req.params.id);
 
-    if(!registeredTrailer) {
+    if (!registeredTrailer) {
       res.status(400).json({
         success: false,
-        msg: 'Trailer not exists'
+        msg: "Trailer not exists",
       });
     }
 
@@ -104,12 +103,11 @@ router.delete('/trailers/:id', user_jwt, async(req, res, next) => {
 
     res.status(200).json({
       success: true,
-      msg: 'Successfully Deleted Trailer'
+      msg: "Successfully Deleted Trailer",
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
-
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,363 +1,379 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const Trailer = require('../models/Trailer');
-const bcryptjs = require('bcryptjs');
-const user_jwt = require('../middleware/user_jwt');
-const jwt = require('jsonwebtoken');
-const { token } = require('morgan');
+const User = require("../models/User");
+const Trailer = require("../models/Trailer");
+const bcryptjs = require("bcryptjs");
+const user_jwt = require("../middleware/user_jwt");
+const jwt = require("jsonwebtoken");
+const { token } = require("morgan");
 
 //회원가입 API
-router.post('/users',async(req, res, next) => {
-    const{ username, email, password, nickname, phone } = req.body;
+router.post("/users", async (req, res, next) => {
+  const { userName, email, password, nickname, phoneNumber } = req.body;
 
-    try {
-        let user_exits = await User.findOne({ email: email })
+  try {
+    let user_exits = await User.findOne({ email: email });
 
-        if(user_exits) {
-            return res.status(400).json({
-                success: false,
-                msg: 'User already exists'
-            });
-        }
-
-        let user = new User();
-
-        user.username = username
-        user.email = email
-
-        const salt = await bcryptjs.genSalt(10);
-        user.password = await bcryptjs.hash(password, salt);
-
-        user.nickname = nickname
-        user.phone = phone
-
-        await user.save();
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        console.log(user.id);
-        console.log(user._id);
-
-        jwt.sign(payload, process.env.jwtUserSecret, {
-            expiresIn: 360000
-        }, (err,token) => {
-            if(err) throw err;
-            res.status(200).json({
-                success: true,
-                token: token
-            });
-        });
-
-    } catch(err) {
-        console.log(err);
+    if (user_exits) {
+      return res.status(400).json({
+        success: false,
+        msg: "User already exists",
+      });
     }
+
+    let user = new User();
+
+    user.userName = userName;
+    user.email = email;
+
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt);
+
+    user.nickname = nickname;
+    user.phoneNumber = phoneNumber;
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    console.log(user.id);
+    console.log(user._id);
+
+    jwt.sign(
+      payload,
+      process.env.jwtUserSecret,
+      {
+        expiresIn: 360000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json({
+          success: true,
+          token: token,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //로그인 API
-router.post('/login', async(req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
+router.post("/login", async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    try {
-        let user = await User.findOne({
-            email: email
-        });
+  try {
+    let user = await User.findOne({
+      email: email,
+    });
 
-        if(!user) {
-            return res.status(400).json({
-                success: false,
-                msg:'User not exists go & register to continue.'
-            });
-        }
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: "User not exists go & register to continue.",
+      });
+    }
 
-    const isMatch = await bcryptjs.compare(password, user.password)
+    const isMatch = await bcryptjs.compare(password, user.password);
 
-    if(!isMatch) {
-        return res.status(400).json({
-            success: false,
-            msg: 'Invalid password'
-        });
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid password",
+      });
     }
 
     const payload = {
-        user: {
-            id: user.id
-        }
-    }
+      user: {
+        id: user.id,
+      },
+    };
 
     jwt.sign(
-        payload, process.env.jwtUserSecret,
-        {
-            expiresIn: 360000
-        }, (err,token) => {
-            if(err) throw err;
+      payload,
+      process.env.jwtUserSecret,
+      {
+        expiresIn: 360000,
+      },
+      (err, token) => {
+        if (err) throw err;
 
-            res.status(200).json({
-                success: true,
-                msg: 'User logged in',
-                token: token,
-                user: user
-            });
-        }
-    )
-
-    } catch(error)  {
-        console.log(error.message);
-        res.status(500).json({
-            success: false,
-            msg: 'Server Error'
-        })
-    }
+        res.status(200).json({
+          success: true,
+          msg: "User logged in",
+          token: token,
+          user: user,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
 //마이페이지 개인정보 호출 API
-router.get('/users', user_jwt, async(req, res, next) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password -__v');
-            res.status(200).json({
-                success: true,
-                user: user
-            });
-    } catch(error) {
-        console.log(error.message);
-        res.status(500).json({
-            success: false,
-            msg: 'Server Error'
-        })
-    }
-})
+router.get("/users", user_jwt, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password -__v");
+    res.status(200).json({
+      success: true,
+      user: user,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
+});
 
 //개인정보 수정 API
-router.put('/users/:id', user_jwt, async(req, res, next) => {
+router.put("/users/:id", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'UserInfo not exists'
+        msg: "UserInfo not exists",
       });
     }
 
     user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true
-    }).select('-password -__v');
+      runValidators: true,
+    }).select("-password -__v");
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'Something went wrong.'
+        msg: "Something went wrong.",
       });
     }
 
     res.status(200).json({
       success: true,
       user: user,
-      msg: 'Successfully updated'
+      msg: "Successfully updated",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //관심 목록 등록 API
-router.put('/users/:id/likeLists', user_jwt, async(req, res, next) => {
+router.put("/users/:id/likeLists", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists'
+        msg: "User not exists",
       });
     }
 
-    user = await User.findByIdAndUpdate(req.params.id, { $push: { likeLists: req.body } }, {
-      new: true,
-    });
+    user = await User.findByIdAndUpdate(
+      req.params.id,
+      { $push: { likeLists: req.body } },
+      {
+        new: true,
+      }
+    );
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'Something went wrong.'
+        msg: "Something went wrong.",
       });
     }
 
     res.status(200).json({
       success: true,
       user: user,
-      msg: 'Successfully likeList registered'
+      msg: "Successfully likeList registered",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //관심 목록 가져오기 API
-router.get('/users/:id/likeLists', user_jwt, async(req, res, next) => {
+router.get("/users/:id/likeLists", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists'
+        msg: "User not exists",
       });
     }
 
-    let likedTrailer = await User.findById(req.params.id).select('-_id -username -email -password -nickname -phone -__v -reservationLists');
+    let likedTrailer = await User.findById(req.params.id).select(
+      "-_id -userName -email -password -nickname -phoneNumber -__v -reservationLists"
+    );
 
-    if(!likedTrailer) {
+    if (!likedTrailer) {
       res.status(400).json({
         success: false,
-        msg: 'likedTrailer not exists.'
+        msg: "likedTrailer not exists.",
       });
     }
 
     res.status(200).json({
       success: true,
       trailer: likedTrailer,
-      msg: 'Successfully likeList registered'
+      msg: "Successfully likeList registered",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //관심 목록 등록 해제 API
-router.delete('/users/:id/likeLists', user_jwt, async(req, res, next) => {
+router.delete("/users/:id/likeLists", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists'
+        msg: "User not exists",
       });
     }
 
-    let likedTrailer = await User.findByIdAndUpdate(req.params.id, { $pull: { likeLists: { trailerId: req.body.trailerId } } });
+    let likedTrailer = await User.findByIdAndUpdate(req.params.id, {
+      $pull: { likeLists: { trailerId: req.body.trailerId } },
+    });
 
-    if(!likedTrailer) {
+    if (!likedTrailer) {
       res.status(400).json({
         success: false,
-        msg: 'LikedTrailer went wrong'
+        msg: "LikedTrailer went wrong",
       });
     }
 
     res.status(200).json({
-      success: true
+      success: true,
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //예약하기 API
-router.put('/users/:id/reservationLists', user_jwt, async(req, res, next) => {
+router.put("/users/:id/reservationLists", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists'
+        msg: "User not exists",
       });
     }
 
-    user = await User.findByIdAndUpdate(req.params.id, { $push: { reservationLists: req.body } }, {
-      new: true,
-    });
+    user = await User.findByIdAndUpdate(
+      req.params.id,
+      { $push: { reservationLists: req.body } },
+      {
+        new: true,
+      }
+    );
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'Something went wrong.'
+        msg: "Something went wrong.",
       });
     }
 
     res.status(200).json({
       success: true,
       user: user,
-      msg: 'Successfully registered'
+      msg: "Successfully registered",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //예약 목록 가져오기 API
-router.get('/users/:id/reservationLists', user_jwt, async(req, res, next) => {
+router.get("/users/:id/reservationLists", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists'
+        msg: "User not exists",
       });
     }
 
-    let reservedTrailer = await User.findById(req.params.id).select('-_id -username -email -password -nickname -phone -__v -likeLists');
+    let reservedTrailer = await User.findById(req.params.id).select(
+      "-_id -userName -email -password -nickname -phoneNumber -__v -likeLists"
+    );
 
-    if(!reservedTrailer) {
+    if (!reservedTrailer) {
       res.status(400).json({
         success: false,
-        msg: 'ReservedTrailer not exists.'
+        msg: "ReservedTrailer not exists.",
       });
     }
 
     res.status(200).json({
       success: true,
       trailer: reservedTrailer,
-      msg: 'Successfully retrieved'
+      msg: "Successfully retrieved",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 //회원 탈퇴 API
-router.delete('/users/:id', user_jwt, async(req, res, next) => {
+router.delete("/users/:id", user_jwt, async (req, res, next) => {
   try {
     let user = await User.findById(req.params.id);
 
-    if(!user) {
+    if (!user) {
       res.status(400).json({
         success: false,
-        msg: 'User not exists '
+        msg: "User not exists ",
       });
     }
 
     //해당 유저가 등록한 트레일러 삭제
     let registeredTrailer = await Trailer.findOne({ userId: req.params.id });
 
-    if(!registeredTrailer) {
+    if (!registeredTrailer) {
       res.status(200).json({
         success: true,
-        msg: 'Registered trailer not exists'
+        msg: "Registered trailer not exists",
       });
     }
 
-    registeredTrailer = await Trailer.findOneAndDelete({ userId: req.params.id });
+    registeredTrailer = await Trailer.findOneAndDelete({
+      userId: req.params.id,
+    });
     user = await User.findByIdAndDelete(req.params.id);
 
     res.status(200).json({
       success: true,
-      msg: 'Successfully Deleted User'
+      msg: "Successfully Deleted User",
     });
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
